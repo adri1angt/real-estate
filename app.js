ab5fea8c1512280a476c38db84dc7a8a9a01317c
adri1angt <adr.angot@gmail.com>
2017-03-03 09:21:00 +0100

    udapte ws3

--- app.js
+++ app.js
@@ -25,3 +25,128 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+app.get('/process', function(req, res) {
+	const url = req.query.lbcUrl;
+
+	if(url) {
+		getLBCData(url, res, getMAEstimation)
+	}
+	else {
+		res.render('home', {
+			error: 'Url is empty'
+		});
+	}
+});
+
+function getLBCData (lbcUrl, routeResponse, callback)
+{
+	request(lbcUrl, function(error, response, html)
+	{	
+		if(!error){
+			
+			let $ = cheerio.load(html); //module pour parser le document html
+			
+			const lbcData = parseLBCData(html)
+			
+			if(lbcData){
+				console.log('LBC DATA:', lbcData) //affiche dans la console
+				callback(lbcData, routeResponse)
+			}
+			
+			else{
+				routeResponse.render('pages/index', {
+					error: 'No Data Found'
+				});
+			}
+		}
+		else{
+			routeResponse.render('pages/index', {
+			error: 'Error loading the given URL'
+			});
+		}	
+		
+	});
+}
+
+function parseLBCData(html) {
+	const $ = cheerio.load(html)
+	const lbcDataArray = $('section.properties span.value')
+	return lbcData = {
+		price: parseInt($(lbcDataArray.get(0)).text().replace(/\s/g,''), 10),
+		city: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\_|s/g,'').replace(/\-\d+/,''),
+		postalCode: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\D|\-/g,''),
+		type: $(lbcDataArray.get(2)).text().trim().toLowerCase(),
+		surface: parseInt($(lbcDataArray.get(4)).text().replace(/\s/g,''), 10)				
+	}
+}
+
+function getMAEstimation(lbcData, routeResponse) {
+	if (lbcData.city
+		& lbcData.postalCode
+		& lbcData.surface
+		& lbcData.price) {
+		const url = 'https://www.meilleursagents.com/prix-immobillier/(city)-(postalCode)/'.replace('(city)', lbcData.city.replace(/\_/g,'')).replace('(postalCode)', lbcData.postalCode);
+
+		console.log('MA URL:', url)
+
+		request(url, function(error, response, html)
+		{
+			if(!error)
+			{
+				let $ = cheerio.load(html);
+				
+				console.log( $( 'meta[name=description]' ).get());
+				console.log( $( 'meta[name=description]' ).get()[0].attribs);
+				console.log( $( 'meta[name=description]' ).get()[0].attribs.content);
+				if ($('meta[name=description]').get().length === 1 && $('meta[name=description]').get()[0].attribs && $('meta[name=description]').get()[0].attribs.content)
+				{
+					const maData = parseMADATA ($('meta[name=description]').get()[0].attribs.content);
+					console.log( 'MA Data:' , maData)
+					
+					if ( maData.priceAppart && maData.priceHouse)
+					{
+						routeResponse.render('home',{
+							data: {
+								ldcData,
+								maData,
+								deal: {
+								good: isgooddeal( lbcData, maData)
+								}
+							}
+						})
+					}	
+				}
+			}	
+		})  
+
+	}
+}
+
+
+function isGoodDeal (lbcData, maData) {
+	/*
+	const adPricePerSqM = Math.round( lbcData.price / lbcData.surface )
+    const maPrice = lbcData.type === 'appartement' ? maData.priceAppart : maData.priceHouse
+
+    return adPricePerSqM < maPrice*/
+}
+
+function parseMAData( html ) {
+
+    const priceAppartRegex = /\bappartement\b : (\d+) €/mi
+    const priceHouseRegex = /\bmaison\b : (\d+) €/mi
+
+    if ( html ) {
+        const priceAppart = priceAppartRegex.exec( html ) && priceAppartRegex.exec( html ).length === 2 ? priceAppartRegex.exec( html )[1] : 0
+        const priceHouse = priceHouseRegex.exec( html ) && priceAppartRegex.exec( html ).length === 2 ? priceHouseRegex.exec( html )[1] : 0
+
+        return {
+            priceAppart: parseInt( priceAppart, 10 ),
+            priceHouse: parseInt( priceHouse, 10 )
+        }
+    }
+
+    return {}
+
+}
